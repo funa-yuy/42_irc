5fa957889fc7dae9f2d4633bb68751254cb0ea90
yohanata42 <yoshikohatanaka1998@gmail.com>
2025-08-06 22:50:28 +0900

    add constructor

--- includes/Parser.hpp
+++ includes/Parser.hpp
@@ -1,12 +1,26 @@
 #ifndef PARSER_HPP
  #define PARSER_HPP
 
+ #include <string>
+
 class Parser
 {
 	public:
-
+		Parser();
+		Parser(std::string line);
+		Parser(const Parser& src);
+		Parser& operator=(const Parser& src);
+		~Parser();
+		typedef struct s_parsed
+		{
+			int client_fd;
+			std::string command;
+			std::string args;
+			std::string msg;
+		}	t_parsed;
+		t_parsed exec();
 	private:
-
+		std::string _line;
 };
 
 #endif
--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,2 +1,26 @@
 # include "Server.hpp"
+# include "Parser.hpp"
 
+Parser::Parser() {}
+
+Parser::Parser(std::string line) :_line(line) {}
+
+Parser::Parser(const Parser& src)
+{
+	_line = src._line;
+}
+
+Parser& Parser::operator=(const Parser& src)
+{
+	_line = src._line;
+	return (*this);
+}
+
+Parser::~Parser() {}
+
+//TODO 構造体の定義を別の場所にする
+Parser::t_parsed Parser::exec()
+{
+	// 一行ずつくる
+	// コマンド＋引数＋メッセージ本体にわける
+}
