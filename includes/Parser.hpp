9bc5296453d0759189f39d86fc11192724151f04
Yoshiko Hatanaka <yohatana@c6r3s3.42tokyo.jp>
2025-08-08 17:41:42 +0900

    think vector

--- includes/Parser.hpp
+++ includes/Parser.hpp
@@ -2,6 +2,32 @@
  #define PARSER_HPP
 
  #include <string>
+ #include <iostream>
+ #include <sstream>
+ #include <vector>
+
+ // コマンド呼び出し側と共有する
+ typedef struct s_parsed
+{
+	int client_fd;
+	std::string command;
+	std::string args;
+	std::string msg;
+}	t_parsed;
+
+typedef enum
+{
+	PASS,
+	NICK,
+	USER,
+	JOIN,
+	PRIVMSG,
+	OPER,
+	KICK,
+	INVITE,
+	TOPIC,
+	MODE
+}	cmds_l;
 
 class Parser
 {
@@ -11,15 +37,11 @@ class Parser
 		Parser(const Parser& src);
 		Parser& operator=(const Parser& src);
 		~Parser();
-		typedef struct s_parsed
-		{
-			int client_fd;
-			std::string command;
-			std::string args;
-			std::string msg;
-		}	t_parsed;
 		// t_parsed exec(); // インスタンス化しないなら要らん
 		t_parsed exec(std::string line, int client_fd);
+		t_parsed return_parse_err(std::string errmsg);
+		t_parsed split_line(std::string line);
+		std::string pick_cmd(std::string line);
 	private:
 		std::string _line;
 };
--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,9 +1,9 @@
-# include "Server.hpp"
-# include "Parser.hpp"
+# include "../includes/Server.hpp"
+# include "../includes/Parser.hpp"
 
 Parser::Parser() {}
 
-Parser::Parser(std::string line) :_line(line) {}
+// Parser::Parser(std::string line) :_line(line) {}
 
 Parser::Parser(const Parser& src)
 {
@@ -18,13 +18,44 @@ Parser& Parser::operator=(const Parser& src)
 
 Parser::~Parser() {}
 
+int main()
+{
+	Parser parser;
+	parser.exec("PRIVMSG B :こんにちは\r\n", 4);
+	return (0);
+}
+
 //TODO 構造体の定義を別の場所にする
-//todo これどうやって使うつもり？インスタンスにするメリットなくね？
-Parser::t_parsed Parser::exec(std::string line, int client_fd)
+//todo インスタンスにするメリットがないのでサバと相談
+/*
+	メッセージ例）
+	PRIVMSG B :こんにちは\r\n
+*/
+t_parsed Parser::exec(std::string line, int client_fd)
 {
 	std::string cmd;
 	std::string args;
 	std::string msg;
+	std::stringstream stream(line);
+
+	std::cout << "line:" << line;
+
+	// vectorで空白スプリットを保管する
+	std::vector<std::string> v;
+	// std::string word;
+	std::string str;
+	while (getline(stream, str, ' '))
+		v.push_back(str);
+
+		for (const auto& w : v) {
+		std::cout << "[" << w << "]" << std::endl;
+	}
+
+	// 先頭はコマンド（かプレフィックス）、最後は本文になる
+	// 間の部分をすべて引数としたvectorで流す
+
+	// stream << line;
+	// std::cout << stream.str() << std::endl;
 
 	// 動的に確保したほうがいい？freeする手間があるので微妙か？
 	t_parsed parsed;
@@ -36,22 +67,42 @@ Parser::t_parsed Parser::exec(std::string line, int client_fd)
 	// ncコマンドで接続してたら必要そう
 	if (512 < len)
 	{
-		std::cout << "" << std::endl;
+		std::cout << "irc msg length over" << std::endl;
 		return (parsed);
 	}
+
 	// コマンド＋引数＋メッセージ本体にわける
+
 	// コマンドリスト（enum）と比較
 	// 存在しないコマンドの場合はDEBUGlogに出力
 
 }
 
-Parser::t_parsed Parser::return_parse_err(std::string errmsg)
+t_parsed Parser::return_parse_err(std::string errmsg)
 {
-	std::cout << errmsg << std::endl;
-	return (NULL);
+	// エラーよけ　そもそも返却する必要あるのか？
+	t_parsed parsed;
+	std::cerr << errmsg << std::endl;
+	return (parsed);
 }
 
 void print_debug(std::string msg)
 {
-	std::cout << "[DEBUG]" << msg << std::endl;
+	std::cerr << "[DEBUG] " << msg << std::endl;
+}
+
+// lineってここで開放する必要ある？
+t_parsed Parser::split_line(std::string line)
+{
+	t_parsed parsed;
+
+	return (parsed);
+}
+
+std::string Parser::pick_cmd(std::string line)
+{
+	std::string cmd;
+	// 空白で分ける
+
+	return (cmd);
 }
