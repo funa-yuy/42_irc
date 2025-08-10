b19cf4b08654e0756dc8e67d2ac98f5090f6d5eb
yohanata42 <yoshikohatanaka1998@gmail.com>
2025-08-10 17:37:26 +0900

    add test

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -23,86 +23,94 @@ int main()
 {
 	Parser parser;
 	t_parsed result;
+	std::vector<std::string> expect_v;
 
 	// test
 	result = parser.exec("PRIVMSG B :こんにちは\r\n", 4);
+	expect_v.push_back("B");
 	assert(result.cmd == "PRIVMSG");
-	assert();
+	assert(result.args[0] == expect_v[0]);
 	assert(result.msg == "こんにちは");
+	expect_v.clear();
 
+	// no msg
 	result = parser.exec("PASS hunter2\r\n", 4);
-	result = parser.exec("NICK nusu\r\n", 4);
-	result = parser.exec("USER nusu 0 * :Nusu Realname\r\n", 4);
-	result = parser.exec("JOIN #chatroom\r\n", 4);
-	result = parser.exec("PRIVMSG #chatroom :Hello everyone!\r\n", 4);
-	result = parser.exec("OPER admin secretpass\r\n", 4);
-	result = parser.exec("KICK #chatroom user123 :Spamming\r\n", 4);
-	result = parser.exec("INVITE user123 #chatroom\r\n", 4);
-	result = parser.exec("TOPIC #chatroom :New topic here!\r\n", 4);
-	result = parser.exec("MODE #chatroom +o user123\r\n", 4);
+	expect_v.push_back("hunter2");
+	assert(result.cmd == "PASS");
+	assert(result.args[0] == expect_v[0]);
+	assert(result.msg == "");
+	expect_v.clear();
 
+	// multi
+	result = parser.exec("USER nusu 0 * :Nusu Realname\r\n", 4);
+	expect_v.push_back("nusu");
+	expect_v.push_back("0");
+	expect_v.push_back("*");
+	assert(result.cmd == "USER");
+	for(int i = 0; i < (int)expect_v.size();i++)
+		assert(result.args[i] == expect_v[i]);
+	assert(result.msg == "Nusu Realname");
+	expect_v.clear();
+
+	result = parser.exec("MODE #chatroom +o user123\r\n", 4);
+	expect_v.push_back("#chatroom");
+	expect_v.push_back("+o");
+	expect_v.push_back("user123");
+	assert(result.cmd == "MODE");
+	for(int i = 0; i < (int)expect_v.size();i++)
+		assert(result.args[i] == expect_v[i]);
+	assert(result.msg == "");
+	expect_v.clear();
+
+	// space only msg
+	result = parser.exec("PRIVMSG B :  \r\n", 4);
+	expect_v.push_back("B");
+	assert(result.cmd == "PRIVMSG");
+	for(int i = 0; i < (int)expect_v.size();i++)
+		assert(result.args[i] == expect_v[i]);
+	assert(result.msg == "  ");
+	expect_v.clear();
+
+	// TODO 以下のケースはどういったデータを返すべきか？
 	// error
 	result = parser.exec("PRIVMSG B B B B B B B B B B B B B B B B:こんにちは\r\n", 4);
-
-	// 許容する？リファレンス確認
+	// 許容する？
 	result = parser.exec("NICK         nusu\r\n", 4);
 	result = parser.exec("PRIVMSG B :\r\n", 4);
 	return (0);
 }
 
-/*
-	メッセージ例）
-	PRIVMSG B :こんにちは\r\n
-*/
 // TODO vectorのメモリリークについて調べる
 t_parsed Parser::exec(std::string line, int client_fd)
 {
 	t_parsed parsed;
 	std::stringstream stream(line);
 	std::vector<std::string> v;
-	std::string str;
 
-	// 後ろから:始まりの部分を本文として抜き出す
-	// 本文がないコマンドの場合はどうなる？
 	int pos = line.rfind(":", line.size() - 1);
-	std::cout << "pos " << pos << std::endl;
 	if (pos < 0)
 		parsed.msg = "";
 	else
 	{
 		parsed.msg = line.substr(pos);
-		// trim /r/n && trim :
+		parsed.msg.erase(0, 1);
+		parsed.msg.erase(parsed.msg.size() - 2, 2);
 		line.erase(pos);
 	}
 
-	// vectorで空白スプリットを保管する
-	while (getline(stream, str, ' '))
-		v.push_back(str);
-
-	// 確認用
-	for (int i = 0; i < v.size();i++) {
-		std::cout << "[" << v[i] << "]" << std::endl;
-	}
-
-	// 先頭はコマンド（かプレフィックス）、最後は本文になる
-	// 間の部分をすべて引数としたvectorで流す
-	parsed.cmd = v[0];
-	// vectorの先頭と末尾を削除
-	v.erase(v.end());
-	v.erase(v.begin());
-
-	std::cout << "cmd: " << parsed.cmd << std::endl;
-	std::cout << "msg: " << parsed.msg << std::endl;
-
-	// コマンドリスト（enum）と比較　// サーバーで担保する
-
-	// 引数の数は15まで許容する
-	if (15 < v.size())
+	std::string temp;
+	while (getline(stream, temp, ' '))
 	{
-		print_debug("too many args");
+		if (temp.size() >= 2 && temp.compare(temp.size() - 2, 2, "\r\n") == 0)
+			temp.erase(temp.size() - 2);
+		v.push_back(temp);
 	}
+	parsed.cmd = v[0];
+	v.erase(v.begin());
+	if (15 < v.size())
+		print_debug("too many args");
 	parsed.args = v;
-
+	parsed.client_fd = client_fd;
 	return (parsed);
 }
 
