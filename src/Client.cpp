cfb8ad0109fa63cb2ec8623ae0c2ee57444bdb71
kenrio <kenken7272721@gmail.com>
2025-09-13 18:48:01 +0900

    fix(client): initialize lastPingTime and lastPingToken in constructor

--- src/Client.cpp
+++ src/Client.cpp
@@ -4,7 +4,8 @@ Client::Client()
 :	_fd(0), _buffer(""),
 	_nickname(""), _username(""), _realname(""),
 	_passReceived(false), _nickReceived(false), _userReceived(false),
-	_isRegistered(false), _isOperator(false)
+	_isRegistered(false), _isOperator(false),
+	_lastPingTime(0), _lastPingToken("")
 {
 	_pfd.fd = 0;
 	_pfd.events = 0;
