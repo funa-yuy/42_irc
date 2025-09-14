dde72746a9d72905ce951b528b920e7f8f462068
funa <orz.home35@gmail.com>
2025-09-14 16:54:08 +0900

    🛠️ エラー処理をつい亜k

--- includes/Command/JoinCommand.hpp
+++ includes/Command/JoinCommand.hpp
@@ -28,6 +28,7 @@ class JoinCommand : public Command {
 		const std::vector<t_response>	executeJoin(const t_parsed& input, Database& db, std::vector<s_join_item> items) const;
 		bool							is_validCmd(const t_parsed& input, t_response* res, Database& db, const s_join_item& item) const;
 		std::vector<s_join_item>		parse_join_args(const t_parsed& input) const;
+		void							updateDatabase(const t_parsed& input, Database& db, const s_join_item& item) const;
 		t_response						makeJoinBroadcast(const t_parsed& input, Database& db, Channel* channel) const;
 		t_response						makeRplTopic(const t_parsed& input, Channel* channel) const;
 		t_response						makeRplNamreply(const t_parsed& input, Database& db, Channel* channel) const;
