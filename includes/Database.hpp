f18a24b61407033cddfbfbcc6071c2476b00e325
kenrio <kenken7272721@gmail.com>
2025-09-21 16:47:27 +0900

    feat(Database): ニックネームから Client を取得するメソッドを追加
    - Database.hpp に getClient(std::string &nickname) を宣言
    - INVITE コマンド実装でニックネーム指定のユーザ検索に利用

--- includes/Database.hpp
+++ includes/Database.hpp
@@ -30,6 +30,7 @@ public:
 
 	Client *			getClient(int fd);
 	Client const *		getClient(int fd) const;
+	Client *			getClient(std::string & nickname);
 	Channel const *		getChannel(const std::string& name) const;
 	Channel *			getChannel(const std::string& name);
 	const std::string&	getPassword() const;
